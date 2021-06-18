069ada87a6965c78847ab9bb9ba65e447b2ecbc4
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-18 13:01:34 +0700

    prepaire project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+
+import { HeaderComponent } from '@layouts/header/header.component';
+import { FooterComponent } from '@layouts/footer/footer.component';
+import { MenuBarComponent } from '@layouts/menu-bar/menu-bar.component';
+import { LogInComponent } from '@layouts/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuBarComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
