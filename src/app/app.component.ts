d3f0ac92eefd90faeaa03ba9327e11c6b50140e8
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-20 19:26:24 +0700

    hide layout before user will login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebApiService } from '@services/api-global/web-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fullstack-front-end-angular-adminlte';
+  constructor(
+    public webapiservice : WebApiService
+  ) {
+
+  }
 }
+
+
