6d7b2f1b06aac405617a75b93fa0623deeab9ca2
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-20 14:38:18 +0700

    add promise function for login service

--- src/services/api-global/web-api.service.ts
+++ src/services/api-global/web-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '@env/environment';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class WebApiService {
 
   login(action :string , value : NgForm){
     const _url = this.url + action;
-    return this.httpClient.post(_url, value, {headers : this.headers}).toPromise();
+    return this.httpClient.post<any>(_url, value, {headers : this.headers}).toPromise();
   }
 
 }
